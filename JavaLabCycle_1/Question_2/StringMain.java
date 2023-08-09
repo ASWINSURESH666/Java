1a96665a648745e54374e214387ed41e7d57ac6a
AswinSuresh666 <117917680+ASWINSURESH666@users.noreply.github.com>
2023-08-09 11:11:12 +0530

    Create StringMain.java

---
+++ JavaLabCycle_1/Question_2/StringMain.java
@@ -0,0 +1,22 @@
+package QUESTION_2;
+
+/**
+ *
+ * @author HP
+ */
+public class StringsMain {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Usage: java cusat.q2.Question_2 <range> <name>");
+            System.exit(1);
+        }
+
+        int num = Integer.parseInt(args[0]);
+        String name = args[1];
+
+        Strings a = new Strings();
+        a.setData(num, name);
+        a.print();
+    }
+}
+
