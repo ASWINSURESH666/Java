0f9b0c8828155b428c47bf56d194159075d18b56
Aswin_SuresH <117917680+ASWINSURESH666@users.noreply.github.com>
2023-10-14 17:26:00 +0530

    Update StringMain.java

--- JavaLabCycle_1/Question_2/StringMain.java
+++ JavaLabCycle_1/Question_2/StringMain.java
@@ -1,18 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package QUESTION_2;
+
+/**
+ *
+ * @author HP
+ */
+
+import java.util.Scanner;
 
 public class StringsMain {
     public static void main(String[] args) {
-        if (args.length != 2) {
-            System.out.println("Usage: java cusat.q2.Question_2 <range> <name>");
-            System.exit(1);
-        }
-
-        int num = Integer.parseInt(args[0]);
-        String name = args[1];
-
-        Strings a = new Strings();
-        a.setData(num, name);
-        a.print();
-    }
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter the number of times to repeat the string: ");
+            String nStr = scanner.nextLine();
+            int n;
+            try {
+                n = Integer.parseInt(nStr);
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Please enter a valid integer.");
+                scanner.close();
+                return;
+            }
+            System.out.print("Enter the string to be repeated: ");
+            String str = scanner.nextLine();
+            Strings.repeatString(str, n);
+            }
 }
-
+}
