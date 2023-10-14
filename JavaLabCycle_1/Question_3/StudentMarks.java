170f3e4d28a1a1c2155463b2a8e1c25282216559
Aswin_SuresH <117917680+ASWINSURESH666@users.noreply.github.com>
2023-10-14 18:31:47 +0530

    Update StudentMarks.java

--- JavaLabCycle_1/Question_3/StudentMarks.java
+++ JavaLabCycle_1/Question_3/StudentMarks.java
@@ -1,49 +1,35 @@
 package QUESTION_3;
+
 import java.util.Scanner;
 
+public class Student {
+    private final String name;
+    private final int rollNumber;
+    private final int[] subjectMarks;
+    private int total;
 
-public class StudentMarks {
-    
-}
+    public Student(String name, int rollNumber, int[] subjectMarks) {
+        this.name = name;
+        this.rollNumber = rollNumber;
+        this.subjectMarks = subjectMarks;
+        calculateTotalMarks();
+    }
 
-class student {
-    String name;
-    int num;
-    int array[] = new int[5];
-    int total;
-    
-    void getDetails(){
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Name of the Student: ");
-        name=sc.nextLine();
-        
-        System.out.print("Enter the Roll Number: ");
-        num=sc.nextInt();
-        
-        for(int i=0;i<5;i++){
-            System.out.print("SUBJECT "+(i+1)+" : ");
-            array[i] = sc.nextInt();
+    private void calculateTotalMarks() {
+        int sum = 0;
+        for (int marks : subjectMarks) {
+            sum += marks;
         }
+        total = sum;
     }
-    
-    void Calculatetotalmarks(){
-        total=0;
-        for (int i=0;i<5;i++){
-            total = total+array[i];
-        }  
-    }
-    
-    int getTotalMark(){
+
+    public int getTotal() {
         return total;
     }
-    
-    void printdetails(){
-        System.out.println("Name of the Student: "+name);
-        System.out.println("Roll Number of the Student: "+num);
-        System.out.println("Marks of the Student: "+total);
-        for(int i=0;i<5;i++){
-            System.out.println("Marks "+(i+1)+" : "+array[i]);
-        }
-        
+
+    public void printDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Total Marks: " + total);
     }
 }
